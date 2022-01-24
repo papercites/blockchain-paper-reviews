0f4daf4613a8d740922f8a886bba42821d30b575
jacklightChen <1797079433@qq.com>
2022-01-24 11:26:05 +0800

    web dev

--- src/components/GithubButton.js
+++ src/components/GithubButton.js
@@ -1,6 +1,6 @@
 function GithubButton() {
     return (
-        <iframe src="https://ghbtns.com/github-btn.html?user=PaperReviews&repo=blockchain-paper-reviews&type=star&count=true&size=large"
+        <iframe src="https://ghbtns.com/github-btn.html?user=papercites&repo=blockchain-paper-reviews&type=star&count=true&size=large"
                 frameBorder="0" scrolling="0" width="140" height="30" title="GitHub" style={{paddingLeft: '5px'}}></iframe>
     );
 
--- src/components/PaperTable.js
+++ src/components/PaperTable.js
@@ -1,9 +1,12 @@
 import './PaperTable.css'
-import {Table, Tag} from "antd";
+import {Table, Tag, Checkbox, Row, Col} from "antd";
 // import Axios from 'axios';
+import {LinkOutlined} from '@ant-design/icons';
 import React from 'react'
 import Axios from "axios";
 import yaml from "js-yaml";
+
+// const plainOptions = ['Blockchain Performance', 'Blockchain Security', 'Blockchain Analysis', 'Blockchain Application'];
 
 class PaperTable extends React.Component {
     constructor(props) {
@@ -11,16 +14,18 @@ class PaperTable extends React.Component {
         this.state = {
             publicPath: '/',
             dataSource: [],
+            showSource: [],
             columns: [
                 {
                     dataIndex: 'paper',
                     key: 'paper',
                     render: (text, record) => (
                         <div>
-                            <a href={record.url} style={{fontSize: '16px'}}>{record.title}</a><br/>
+                            <a href={record.url} style={{fontSize: '16px'}}>{record.title}<LinkOutlined/></a><br/>
                             <span>{record.authors}</span><br/>
-                            <span>{record.year}</span><br/>
-                            <Tag color="magenta">tag1</Tag> <Tag color="volcano">tag2</Tag>
+                            <span>{record.conf} {record.year}</span><br/>
+                            <TagFormat tag={record.tag}/>
+                            <SubTagFormat subtag={record.subtag}/>
                         </div>
                     )
                 }
@@ -36,19 +41,84 @@ class PaperTable extends React.Component {
             }
             _this.setState(
                 {
-                    dataSource: allpapers
+                    dataSource: allpapers,
+                    showSource: allpapers
                 },
             )
         })
     }
+    onChange(checkedValues) {
+        if(checkedValues.length>0){
+            this.setState(
+                {
+                    showSource: this.state.dataSource.filter(function (item){
+                        return checkedValues.indexOf(item.tag)>=0
+                    })
+                },
+            )
+        }else{
+            this.setState(
+                {
+                    showSource: this.state.dataSource
+                }
+            )
+        }
+    }
     render() {
         return (
             <section>
-                <Table dataSource={this.state.dataSource} columns={this.state.columns} showHeader={false}>
+                <Checkbox.Group style={{ width: '100%', paddingTop: '10px', paddingBottom: '10px' }} onChange={this.onChange.bind(this)}>
+                    <Row>
+                        <Col span={6}>
+                            <Checkbox value="performance">Blockchain Performance</Checkbox>
+                        </Col>
+                        <Col span={6}>
+                            <Checkbox value="security">Blockchain Security</Checkbox>
+                        </Col>
+                        <Col span={6}>
+                            <Checkbox value="analysis">Blockchain Analysis</Checkbox>
+                        </Col>
+                        <Col span={6}>
+                            <Checkbox value="application">Blockchain Application</Checkbox>
+                        </Col>
+                    </Row>
+                </Checkbox.Group>
+                <Table dataSource={this.state.showSource} columns={this.state.columns} showHeader={false}>
                 </Table>
             </section>
         );
     }
+}
+
+function TagFormat(props) {
+    if(props){
+        switch (props.tag){
+            case 'performance':
+                return  <Tag color="magenta">{props.tag}</Tag>;
+            case 'security':
+                return  <Tag color="cyan">{props.tag}</Tag>;
+            case 'analysis':
+                return  <Tag color="blue">{props.tag}</Tag>;
+            case 'application':
+                return  <Tag color="purple">{props.tag}</Tag>;
+            default:
+                return  null;
+        }
+    }
+    return null;
+}
+
+function SubTagFormat(props) {
+    if(props){
+        for(let i=0;i<props.subtag.length;i++){
+            switch (props.subtag[i]){
+                default:
+                    return  <Tag color="default">{props.subtag[i]}</Tag>;
+            }
+        }
+
+    }
+    return null;
 }
 
 export default PaperTable;
